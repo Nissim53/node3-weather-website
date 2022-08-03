87ddd1986b8fae9941cfe4813e090b3d31894774
Nissim Elkabets-Rosen <nissim_elkabets_rosen@yahoo.com>
2022-08-03 19:06:57 +0200

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (longtitude, latitude, callback) => {
-    const url = "http://api.weatherstack.com/current?access_key=a3645b98ff02695806ac45aad382b5d0&query=" + latitude + "," + longtitude + "&units=f"
+    const url = "http://api.weatherstack.com/current?access_key=a3645b98ff02695806ac45aad382b5d0&query=" + latitude + "," + longtitude
 
     request({ url, json: true}, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (longtitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search', undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out, it feels like ${body.current.feelslike} degresse out`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out, it feels like ${body.current.feelslike} degresse out, ${body.current.humidity}% humidity`)
         }
     })
 }
